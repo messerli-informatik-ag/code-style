bb4788582bae5843faacc28cc74af33d098c4d01
chrisvonfelten <chris.vonfelten@messerli.ch>
2021-05-12 15:15:43 +0200

    Inline constants to where they are used

--- CodeStyle.Analyzers/InterfaceAnalyzers/PublicModifier/RuleConstants.cs
+++ CodeStyle.Analyzers/InterfaceAnalyzers/PublicModifier/RuleConstants.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.CodeAnalysis;
-using static Messerli.CodeStyle.Analyzers.InterfaceAnalyzers.PublicModifier.ConstantsProperties;
 
 namespace Messerli.CodeStyle.Analyzers.InterfaceAnalyzers.PublicModifier
 {
     public static class RuleConstants
     {
+        private static readonly LocalizableString Title = "Interface method declaration contains public access modifiers";
+        private static readonly LocalizableString MessageFormat = "Method contains public access modifiers";
+        private static readonly LocalizableString Description = "Public access modifiers should all be omitted on interface method declaration.";
+        private const string Category = "Access Modifiers";
+
         public const string DiagnosticId = "MESSERLI001";
 
         public static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
